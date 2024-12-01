20362167795f85b555d0774199d2d287ad5e0758
tiagopatzz <tiago.patzlaff@universo.univates.br>
2024-12-01 20:23:16 +0000

    Save changes made directly on the VM

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = "postgresql://univates:univates@localhost/sistema_eventos"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
